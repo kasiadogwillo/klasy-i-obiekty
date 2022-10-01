41b38ded7f1d854ed7c69d5b10d921f053a7e584
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-10-01 17:41:27 +0200

    commiting .gitignore klasy i obiekty

---
+++ src/KlasyObiekty.java
@@ -0,0 +1,24 @@
+public class KlasyObiekty {
+    public static void main(String[] args) {
+
+        //co to jest klasa
+            //template wykorzystywany do tworzenia podobnych do siebie obiektów
+
+        //elementy składowe klasy
+            //1. pola obiektu = z czego będzie składał się dany obiekt, np. co poże posiadać user?
+
+        //tworząc nową klasę, tworzymy typ obiektu
+        //typ = nazwa klasy
+
+        //obiekt
+        //typ obiektu - nazwa klasy (User) = new nazwa klasy()
+        User kasia = new User(); //utworzono nowy obiekt typu
+        System.out.println(kasia.firstName);
+
+        User tomek = new User();
+        System.out.println(tomek.email);
+
+
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,13 @@
+public class User {
+
+        //co to jest klasa
+        //template wykorzystywany do tworzenia podobnych do siebie obiektów
+
+        //elementy składowe klasy
+        //1. pola obiektu = z czego będzie składał się dany obiekt, np. co poże posiadać user?
+
+        String firstName;
+        String lastName;
+        String email;
+        int wiek;
+    }
