1046a3d5eb8c48c4675f88f7d10776683ae93565
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-10-01 18:01:47 +0200

    commiting .gitignore klasy i obiekty

--- src/KlasyObiekty.java
+++ src/KlasyObiekty.java
@@ -12,13 +12,46 @@ public class KlasyObiekty {
 
         //obiekt
         //typ obiektu - nazwa klasy (User) = new nazwa klasy()
-        User kasia = new User(); //utworzono nowy obiekt typu
-        System.out.println(kasia.firstName);
+//        User kasia = new User(); //utworzono nowy obiekt typu
+//        kasia.firstName = "Dogwiłło";
+//        System.out.println(kasia.firstName);
+//
+//        User tata = new User();
+//        tata.email = "tata@email.com";
+//        System.out.println(tata.email);
+//
+//        User seba = new User();
+//        seba.wiek = 37;
+//        System.out.println(seba.wiek);
+//
+//        User gacek = new User ();
+//        gacek.isAdult = true;
+//        System.out.println(gacek.isAdult);
 
-        User tomek = new User();
-        System.out.println(tomek.email);
+        User bartosz = new User ();
+        bartosz.firstName = "Bartosz";
+        bartosz.lastName = "Wojteczek";
+        bartosz.email = "nieznammailabartosza@gmail.com";
+        bartosz.wiek = 36;
+        bartosz.isAdult = true;
 
+        System.out.println(bartosz.firstName);
+        System.out.println(bartosz.lastName);
+        System.out.println(bartosz.email);
+        System.out.println(bartosz.wiek);
+        System.out.println(bartosz.isAdult);
 
+        User seba = new User ();
+        seba.firstName = "Sebastian";
+        seba.lastName = "Dogwiłło";
+        seba.email = "sdogwillo@gmail.com";
+        seba.wiek = 37;
+        seba.isAdult = false;
 
+        System.out.println(seba.firstName);
+        System.out.println(seba.lastName);
+        System.out.println(seba.email);
+        System.out.println(seba.wiek);
+        System.out.println(seba.isAdult);
     }
 }
